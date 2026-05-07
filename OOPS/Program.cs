4495d861c5fdeb611e8c186e5f4754d78c2cc23c
akash <akash@DESKTOP-B1PDELG>
2026-05-07 16:22:21 +0530

    Add project files.

---
+++ OOPS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+
+    class Counter
+    {
+        private int InstanceCount = 0;
+        private static int StaticCount = 0;
+
+        public void Increment()
+        {
+            InstanceCount++;
+            StaticCount++;
+            Console.WriteLine("Instance Count: " + InstanceCount);
+            Console.WriteLine("Static Count: " + StaticCount);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter();
+            Counter counter2 = new Counter();
+
+            Console.WriteLine("Counter 1:");
+            counter1.Increment();
+            
+            counter1.Increment();
+
+            Console.WriteLine("Counter 2:");
+            counter2.Increment();
+            counter2.Increment();
+        }
+    }
+}
