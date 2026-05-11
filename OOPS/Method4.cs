d739e0a54985818efa42471c44151a6b746ba038
akash <akash@DESKTOP-B1PDELG>
2026-05-11 12:36:25 +0530

    Method With Return val and With parameters

---
+++ OOPS/Method4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Lclass
+    {
+        public int sum(int x,int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+    internal class Method4
+    {
+        static void Main(string[] args)
+        {
+            Lclass obj = new Lclass();
+            int sumResult = obj.sum(5, 10);
+            Console.WriteLine("Returned value from sum method: " + sumResult);
+        }
+    }
+}
