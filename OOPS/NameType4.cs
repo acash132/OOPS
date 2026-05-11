9e529f498e8194f6f8a1a0d3b5451199e8a486cf
akash <akash@DESKTOP-B1PDELG>
2026-05-11 12:41:04 +0530

    Display Name with parameters and with return val

---
+++ OOPS/NameType4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Lclass2
+    {
+        public string Name(string name)
+        {
+            return name;
+        }
+    }
+    internal class NameType4
+    {
+        static void Main(string[] args)
+        {
+            Lclass2 obj = new Lclass2();
+            string nameResult = obj.Name("Akash");
+            Console.WriteLine("Returned value from name method: " + nameResult);
+        }
+    }
+}
