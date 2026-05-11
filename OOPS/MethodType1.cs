c9bb41540803c7f2df80618c00732841a09e262c
akash <akash@DESKTOP-B1PDELG>
2026-05-11 11:43:55 +0530

    Void Methods Added

---
+++ OOPS/MethodType1.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class MyClass
+//    {
+//        public void Greet()
+//        {
+//            Console.WriteLine("Hello! Welcome to Methods");
+//        }
+//        public void Add()
+//        {
+//            int a, b, sum;
+//            a = 1;
+//            b = 2;
+//            sum = a + b;
+//            Console.WriteLine("The sum of " + a + " and " + b + " is: " + sum);
+//        }
+//    }
+//    internal class MethodType1
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass myClass = new MyClass();
+//            myClass.Greet();
+//            myClass.Add();
+//        }
+
+//    }
+//}
---
+++ OOPS/MethodType2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Class2   
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition: " + (a + b));
+        }
+        public void Greet(int x)
+        {
+            Console.WriteLine("Hello, " + x + "! Welcome to Methods");
+        }
+
+    }
+    internal class MethodType2
+    {
+        static void Main(string[] args)
+        {
+            Class2 class2 = new Class2();
+            class2.Add(5, 10);
+            class2.Greet(25);
+        }
+    }
+}
