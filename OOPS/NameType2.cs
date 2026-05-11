05a422b670978b454f03f01f8e9c29e8c2e70785
akash <akash@DESKTOP-B1PDELG>
2026-05-11 12:07:45 +0530

    Displaying name  parameterised and parameterless

---
+++ OOPS/NameType2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class myclass2
+    {
+        public void mymethod2(string name)
+        {
+            Console.WriteLine("Hello, " + name + "! This is method with parameters without return type");
+        }
+    }
+    internal class NameType2
+    {
+        static void Main(string[] args)
+        {
+            myclass2 obj = new myclass2();
+            obj.mymethod2("Akash");
+        }
+    }
+}
---
+++ OOPS/Nametype1.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class myclass1
+//    {
+//        public void display()
+//        {
+//            Console.WriteLine("This is Akash");
+//        }
+//    }
+//    internal class Nametype1
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass1 obj = new myclass1();
+//            obj.display();
+//        }
+//    }
+//}
