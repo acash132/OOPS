709f71a2c8fd06ff21063083da53715c1b54f3ea
akash <akash@DESKTOP-B1PDELG>
2026-05-11 12:34:02 +0530

    Displaying name With Rt and Without Parameters

---
+++ OOPS/NameType3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Nclass {
+        public String Method3()
+        {
+            string name = "Akash";
+           
+            return name ;
+        }
+    }
+    internal class NameType3
+    {
+        static void Main(string[] args)
+        {
+            Nclass obj = new Nclass();
+            string nameResult = obj.Method3();
+            Console.WriteLine("Returned value from Method3: " + nameResult);
+        }
+    }
+}
