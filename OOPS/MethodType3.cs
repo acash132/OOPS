77ef232a20e9242dd929f5bc8c6fd7106544116b
akash <akash@DESKTOP-B1PDELG>
2026-05-11 12:25:00 +0530

    method With Return value and No parameters

---
+++ OOPS/MethodType3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class newMethodType3
+    {
+        public int Sum()
+        {
+            int a = 5;
+            int b = 10;
+            int result = a + b;
+            Console.WriteLine("The sum is: " + result);
+            return result;
+        }
+       
+    }
+    internal class MethodType3
+    {
+        static void Main(string[] args)
+        {
+            newMethodType3 obj = new newMethodType3();
+            int sumResult = obj.Sum();
+            Console.WriteLine("Returned value from sum method: " + sumResult);
+        }
+    }
+}
