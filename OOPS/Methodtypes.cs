9b765bab099c4c45f2f51704f43b1739fa0e4e21
akash <akash@DESKTOP-B1PDELG>
2026-05-12 12:03:51 +0530

    Static and Instance Methods Added

---
+++ OOPS/Methodtypes.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace OOPS
+//{
+//    class MethodClass
+//    {
+//        public void Add()
+//        {
+//            int a = 5;
+//            int b = 10;
+//            int sum = a + b;
+//            Console.WriteLine("Sum: " + sum);
+//        }
+//    }
+//    internal class Methodtypes
+//    {
+//        static void Main(string[] args)
+//        {
+//            MethodClass methodObj = new MethodClass();
+//            methodObj.Add();
+//        }
+//    }
+//}
---
+++ OOPS/StaticMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class SM
+    {
+        public static void Subtract()
+        {
+            int a = 20;
+            int b = 10;
+            int difference = a - b;
+            Console.WriteLine("Difference: " + difference);
+        }
+    }
+    internal class StaticMethod
+    {
+        static void Main(string[] args)
+        {
+            SM.Subtract();
+        }
+    }
+}
