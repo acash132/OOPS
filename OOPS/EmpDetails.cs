7b20a41f741a8da3f771448e75f68d9238c646d2
akash <akash@DESKTOP-B1PDELG>
2026-05-13 11:42:51 +0530

    EmployeeDetails

---
+++ OOPS/EmpDetails.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Employee
+    {
+        int Eid;
+        string Ename;
+        int ESalary;
+        static string Company="ABC";
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Enter Employee ID:");
+            Eid = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name:");
+            Ename = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary:");
+            ESalary = int.Parse(Console.ReadLine());
+        }
+
+        public static void DisplayCompany()
+        {
+            Console.WriteLine("Company: " + Company);
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee ID: " + Eid);
+            Console.WriteLine("Employee Name: " + Ename);
+            Console.WriteLine("Employee Salary: " + ESalary);
+            Console.WriteLine("Company: " + Company);
+        }
+    }
+    internal class EmpDetails
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[5];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = new Employee();
+                employees[i].GetDetails();
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i].DisplayDetails();
+            }
+            Employee.DisplayCompany();
+        }
+    }
+}
