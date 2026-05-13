cf54f89ab62dd42cdc5d7967c817719be90035b8
akash <akash@DESKTOP-B1PDELG>
2026-05-13 09:28:29 +0530

    Student Percentage

--- OOPS/StudentDetails.cs
+++ OOPS/StudentDetails.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class Student
-    {
-        public int SId; 
-        public string SName; 
-        public int SAge; 
-        public static string College;//Static member to store college name
+//namespace OOPS
+//{
+//    class Student
+//    {
+//        public int SId; 
+//        public string SName; 
+//        public int SAge; 
+//        public static string College;//Static member to store college name
 
-        public void DisplayDetails()
-        {
-            Console.WriteLine("Student ID: " + SId);
-            Console.WriteLine("Student Name: " + SName);
-            Console.WriteLine("Student Age: " + SAge);
-            Console.WriteLine("College: " + College);
-        }
-    }
-    internal class StudentDetails
-    {
-        static void Main(string[] args)
-        {
-            Student.College = "Osmania University"; // Setting the static member
-            Student[] students = new Student[5]; // Creating an array of Student objects
-            for (int i = 0; i < students.Length; i++)
-            {
-                students[i] = new Student(); // Initializing each Student object
-                Console.WriteLine("Enter details for student " + (i + 1) + ":");
-                Console.Write("ID: ");
-                students[i].SId = int.Parse(Console.ReadLine());
-                Console.Write("Name: ");
-                students[i].SName = Console.ReadLine();
-                Console.Write("Age: ");
-                students[i].SAge = int.Parse(Console.ReadLine());
-            }
+//        public void DisplayDetails()
+//        {
+//            Console.WriteLine("Student ID: " + SId);
+//            Console.WriteLine("Student Name: " + SName);
+//            Console.WriteLine("Student Age: " + SAge);
+//            Console.WriteLine("College: " + College);
+//        }
+//    }
+//    internal class StudentDetails
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student.College = "Osmania University"; // Setting the static member
+//            Student[] students = new Student[5]; // Creating an array of Student objects
+//            for (int i = 0; i < students.Length; i++)
+//            {
+//                students[i] = new Student(); // Initializing each Student object
+//                Console.WriteLine("Enter details for student " + (i + 1) + ":");
+//                Console.Write("ID: ");
+//                students[i].SId = int.Parse(Console.ReadLine());
+//                Console.Write("Name: ");
+//                students[i].SName = Console.ReadLine();
+//                Console.Write("Age: ");
+//                students[i].SAge = int.Parse(Console.ReadLine());
+//            }
 
-            // Displaying details of all students
-            for (int i = 0; i < students.Length; i++)
-            {
-                Console.WriteLine("\nDetails of student " + (i + 1) + ":");
-                students[i].DisplayDetails();
-            }
-        }
-    }
-}
+//            // Displaying details of all students
+//            for (int i = 0; i < students.Length; i++)
+//            {
+//                Console.WriteLine("\nDetails of student " + (i + 1) + ":");
+//                students[i].DisplayDetails();
+//            }
+//        }
+//    }
+//}
---
+++ OOPS/StudentPercentage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class StudentPercentage
+    {
+       
+        class Student
+        {
+            public int SId;
+            public string SName;
+            public int SAge;
+            public double[] Marks = new double[3]; // Array to store 3 subject marks
+            public static string College; // Static member
+
+            public void DisplayDetails()
+            {
+                // Calculating Total
+                double total = 0;
+                foreach (double m in Marks)
+                {
+                    total += m;
+                }
+
+                // Calculating Percentage
+                double percentage = (total / (Marks.Length * 100)) * 100;
+
+                Console.WriteLine("Student ID: " + SId);
+                Console.WriteLine("Student Name: " + SName);
+                Console.WriteLine("Student Age: " + SAge);
+                Console.WriteLine("College: " + College);
+                Console.WriteLine("Total Marks: " + total);
+                Console.WriteLine("Percentage: " + percentage.ToString() + "%");
+            }
+        }
+
+        internal class StudentDetails
+        {
+            static void Main(string[] args)
+            {
+                Student.College = "Osmania University";
+
+                Student[] students = new Student[3];
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    students[i] = new Student();
+                    Console.WriteLine("\n--- Enter details for student " + (i + 1) + " ---");
+
+                    Console.Write("ID: ");
+                    students[i].SId = int.Parse(Console.ReadLine());
+
+                    Console.Write("Name: ");
+                    students[i].SName = Console.ReadLine();
+
+                    Console.Write("Age: ");
+                    students[i].SAge = int.Parse(Console.ReadLine());
+
+                    // Inputting marks
+                    for (int j = 0; j < students[i].Marks.Length; j++)
+                    {
+                        Console.Write("Enter Mark for Subject " + (j + 1) + ": ");
+                        students[i].Marks[j] = double.Parse(Console.ReadLine());
+                    }
+                }
+
+                // Displaying details of all students
+                Console.WriteLine("\n==============================");
+                Console.WriteLine("       STUDENT REPORTS        ");
+                Console.WriteLine("==============================");
+
+                for (int i = 0; i < students.Length; i++)
+                {
+                    Console.WriteLine("\nDetails of student " + (i + 1) + ":");
+                    students[i].DisplayDetails();
+                    Console.WriteLine("------------------------------");
+                }
+
+                
+            }
+        }
+    }
+}
+
