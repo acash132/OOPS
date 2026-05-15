4a008746e72ba4cb94dd9e6d6ce62ff84d7e9aac
akash <akash@DESKTOP-B1PDELG>
2026-05-15 10:27:12 +0530

    Student Parameterised constructor

---
+++ OOPS/StuDetails.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Student
+    {
+         int SID ;
+         string SName ;
+         int SFee ;
+
+        public Student()
+        {
+            SID = 1;
+            SName = "Akash";
+            SFee = 50000;
+        }
+
+        public Student(int Id, string Name, int Fee)
+        {
+            SID = Id;
+            SName = Name;
+            SFee = Fee;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Student ID: " + SID);
+            Console.WriteLine("Student Name: " + SName);
+            Console.WriteLine("Student Fee: " + SFee);
+        }
+
+
+    }
+    internal class StuDetails
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student();
+            Student stu2 = new Student(2, "Sai", 60000);
+            stu1.DisplayDetails();
+            stu2.DisplayDetails();
+        }
+    }
+}
