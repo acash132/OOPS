09cee002b0f3e01fbd5118ed4cb1047f3f2f7581
akash <akash@DESKTOP-B1PDELG>
2026-05-15 11:48:28 +0530

    Car With parameterised constructor

---
+++ OOPS/CarParamConst.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class car
+    {
+         
+         string model;
+         int year;
+         string color;
+        public car()
+        {
+            model = "Honda Civic";
+            year = 2025;
+            color = "Black";
+        }
+        public car(string Model, int Year, string Color)
+        {
+            model = Model;
+            year = Year;
+            color = Color;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Car Model: " + model);
+            Console.WriteLine("Car Year: " + year);
+            Console.WriteLine("Car Color: " + color);
+        }
+
+    }
+    internal class CarParamConst
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            car car2 = new car("Mahindra Scorpio", 2026, "White");
+            car1.DisplayDetails();
+            Console.WriteLine();
+            car2.DisplayDetails();
+        }
+    }
+}
