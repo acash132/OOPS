60fc80616047f82b43cfde50c436baf02cfc6646
akash <akash@DESKTOP-B1PDELG>
2026-05-16 09:25:31 +0530

    Parameterised Constructor Examples

--- OOPS/CarParamConst.cs
+++ OOPS/CarParamConst.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class car
-    {
+//namespace OOPS
+//{
+//    class car
+//    {
          
-         string model;
-         int year;
-         string color;
-        public car()
-        {
-            model = "Honda Civic";
-            year = 2025;
-            color = "Black";
+//         string model;
+//         int year;
+//         string color;
+//        public car()
+//        {
+//            model = "Honda Civic";
+//            year = 2025;
+//            color = "Black";
           
-        }
-        public car(string Model, int Year, string Color)
-        {
-            model = Model;
-            year = Year;
-            color = Color;
-            if (year < 1886)
-            {
-                Console.WriteLine("invalid year for a car,setting year to 1886");
-                year = 1886;
-            }
-            else
-            {
-                year = Year;
-            }
-        }
-        public void DisplayDetails()
-        {
+//        }
+//        public car(string Model, int Year, string Color)
+//        {
+//            model = Model;
+//            year = Year;
+//            color = Color;
+//            if (year < 1886)
+//            {
+//                Console.WriteLine("invalid year for a car,setting year to 1886");
+//                year = 1886;
+//            }
+//            else
+//            {
+//                year = Year;
+//            }
+//        }
+//        public void DisplayDetails()
+//        {
             
-                Console.WriteLine("Car Model: " + model);
-                Console.WriteLine("Car Year: " + year);
-                Console.WriteLine("Car Color: " + color);
+//                Console.WriteLine("Car Model: " + model);
+//                Console.WriteLine("Car Year: " + year);
+//                Console.WriteLine("Car Color: " + color);
             
             
-        }
+//        }
 
-    }
-    internal class CarParamConst
-    {
-        static void Main(string[] args)
-        {
-            car car1 = new car();
-            car car2 = new car("Mahindra Scorpio", 2026, "White");
-            car1.DisplayDetails();
-            Console.WriteLine();
-            car2.DisplayDetails();
-            car car3 = new car("Ford Mustang", 1885, "Red");
-            car3.DisplayDetails();
-        }
-    }
-}
+//    }
+//    internal class CarParamConst
+//    {
+//        static void Main(string[] args)
+//        {
+//            car car1 = new car();
+//            car car2 = new car("Mahindra Scorpio", 2026, "White");
+//            car1.DisplayDetails();
+//            Console.WriteLine();
+//            car2.DisplayDetails();
+//            car car3 = new car("Ford Mustang", 1885, "Red");
+//            car3.DisplayDetails();
+//        }
+//    }
+//}
---
+++ OOPS/PrintConnString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class PrintConnString
+    {
+        
+        class DataBaseConnection
+        {
+            // Field to store the connection string
+            private string ConnectionString;
+
+            // Parameterized Constructor
+            public DataBaseConnection(string connectionString)
+            {
+                // Assigning the parameter value to the class field
+                ConnectionString = connectionString;
+            }
+
+            // Method to print the connection string
+            public void PrintConnectionString()
+            {
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Current Connection String:");
+                Console.WriteLine(ConnectionString);
+                Console.WriteLine("--------------------------------------------------");
+            }
+        }
+
+        internal class ParameterRealEX
+        {
+            static void Main(string[] args)
+            {
+                // 1. Define the connection string
+                string connStr = "Data Source=.;Initial Catalog=UsersDB;Integrated Security=True";
+
+                // 2. Pass the connection string as a parameter to the constructor
+                DataBaseConnection dbConnection = new DataBaseConnection(connStr);
+
+                // 3. Call the method to print it
+                dbConnection.PrintConnectionString();
+            }
+        }
+    }
+}
+
