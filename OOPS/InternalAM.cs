c9a9015c648a412baace66204916e123f49dc442
akash <akash@DESKTOP-B1PDELG>
2026-05-18 10:46:22 +0530

    Internal Access modifier

---
+++ OOPS/InternalAM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class MyClass
+    {
+        internal int a;
+        public MyClass()
+        {
+            a = 10;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Value of a: " + a);
+        }
+
+    }
+    internal class InternalAM
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Display();
+        }
+    }
+}
