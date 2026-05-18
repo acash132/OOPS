2ff0a0bcd0ecf003a3c7576fb910a0c112c90f7d
akash <akash@DESKTOP-B1PDELG>
2026-05-18 11:35:57 +0530

    Protected Access Modifier

--- OOPS/InternalAM.cs
+++ OOPS/InternalAM.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class MyClass
-    {
-        internal int a;
-        public MyClass()
-        {
-            a = 10;
-        }
-        public void Display()
-        {
-            Console.WriteLine("Value of a: " + a);
-        }
+//namespace OOPS
+//{
+//    class MyClass
+//    {
+//        internal int a;
+//        public MyClass()
+//        {
+//            a = 10;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("Value of a: " + a);
+//        }
 
-    }
-    class NewClass
-    {
-        public void Show()
-        {
-            MyClass obj = new MyClass();
-            Console.WriteLine("Accessing internal member a: " + obj.a);
-        }
-    }
-    internal class InternalAM
-    {
-        static void Main(string[] args)
-        {
-            MyClass obj = new MyClass();
-            obj.Display();
-        }
-    }
-}
+//    }
+//    class NewClass
+//    {
+//        public void Show()
+//        {
+//            MyClass obj = new MyClass();
+//            Console.WriteLine("Accessing internal member a: " + obj.a);
+//        }
+//    }
+//    internal class InternalAM
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass obj = new MyClass();
+//            obj.Display();
+//        }
+//    }
+//}
