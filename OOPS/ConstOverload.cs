96c5130af90b1ae4187b1ce146908d5a62970102
akash <akash@DESKTOP-B1PDELG>
2026-05-18 09:56:11 +0530

    Constructor Overloading

---
+++ OOPS/ConstOverload.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Student
+    {
+        int Sid;
+        string Sname;
+        string Sdept;
+        public Student()
+        {
+            Sid = 1;
+            Sname = "Not Provided";
+            Sdept = "Not Provided";
+        }
+        public Student(int sid, string sname, string sdept)
+        {
+            Sid = sid;
+            Sname = sname;
+            Sdept = sdept;
+        }
+        public Student(int sid, string sname)
+        {
+            Sid = sid;
+            Sname = sname;
+            Sdept = "Not Provided";
+        }
+
+        public Student(string sname)
+        {
+            Sid = 0;
+            Sname = sname;
+            Sdept = "Not Provided";
+        }
+        public Student(int sid)
+        {
+            Sid = sid;
+            Sname = "Not Provided";
+            Sdept = "Not Provided";
+        }
+        public void Display()
+        {
+            Console.WriteLine("Student ID: " + Sid);
+            Console.WriteLine("Student Name: " + Sname);
+        }
+        
+        
+    }
+
+    internal class ConstOverload
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Console.WriteLine("Student 1 Details:");
+            student1.Display();
+            Console.WriteLine("\n-----------------------------\n");
+            Student student2 = new Student(101, "Akash");
+            Console.WriteLine("Student 2 Details:");
+            student2.Display();
+            Console.WriteLine("\n-----------------------------\n");
+            Student student3 = new Student("Suresh");
+            Console.WriteLine("Student 3 Details:");
+            student3.Display();
+            Console.WriteLine("\n-----------------------------\n");
+            Student student4 = new Student(104);
+            Console.WriteLine("Student 4 Details:");
+            student4.Display();
+            Console.WriteLine("\n-----------------------------\n");
+            Student student5 = new Student(105, "sai", "CSE");
+            Console.WriteLine("Student 5 Details:");
+            student5.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
--- OOPS/StudParamConst.cs
+++ OOPS/StudParamConst.cs
@@ -1,66 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
+//namespace OOPS
+//{
     
 
-namespace OOPS
-    {
-        class Student
-        {
-            // Fields
-            private int sid;
-            private string sname;
-            private int sage;
+//namespace OOPS
+//    {
+//        class Student
+//        {
+//            // Fields
+//            private int sid;
+//            private string sname;
+//            private int sage;
 
-            // Parameterized Constructor with Age Validation
-            public Student(int id, string name, int age)
-            {
-                sid = id;
-                sname = name;
+//            // Parameterized Constructor with Age Validation
+//            public Student(int id, string name, int age)
+//            {
+//                sid = id;
+//                sname = name;
 
-                // Business Rule: Age must be between 18 and 60
-                if (age >= 18 && age <= 60)
-                {
-                    sage = age;
-                }
-                else
-                {
-                    Console.WriteLine("Setting default age to 18.");
-                    sage = 18; 
-                }
-            }
+//                // Business Rule: Age must be between 18 and 60
+//                if (age >= 18 && age <= 60)
+//                {
+//                    sage = age;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Setting default age to 18.");
+//                    sage = 18; 
+//                }
+//            }
 
-            // Method to display student data
-            public void DisplayStudentDetails()
-            {
-                Console.WriteLine("\n--- Student Details ---");
-                Console.WriteLine("Student ID   : " + sid);
-                Console.WriteLine("Student Name : " + sname);
-                Console.WriteLine("Student Age  : " + sage);
-                Console.WriteLine("-----------------------");
-            }
-        }
+//            // Method to display student data
+//            public void DisplayStudentDetails()
+//            {
+//                Console.WriteLine("\n--- Student Details ---");
+//                Console.WriteLine("Student ID   : " + sid);
+//                Console.WriteLine("Student Name : " + sname);
+//                Console.WriteLine("Student Age  : " + sage);
+//                Console.WriteLine("-----------------------");
+//            }
+//        }
 
-        internal class Program
-        {
-            static void Main(string[] args)
-            {
-                //Test Case 1
-                Console.WriteLine("Creating Student 1 (Valid Age)");
-                Student student1 = new Student(101, "Akash", 24);
-                student1.DisplayStudentDetails();
+//        internal class Program
+//        {
+//            static void Main(string[] args)
+//            {
+//                //Test Case 1
+//                Console.WriteLine("Creating Student 1 (Valid Age)");
+//                Student student1 = new Student(101, "Akash", 24);
+//                student1.DisplayStudentDetails();
 
-                // Test Case 2
-                Console.WriteLine("\nCreating Student 2 (Invalid Age)");
-                Student student2 = new Student(102, "Modi", 75);
-                student2.DisplayStudentDetails();
-            }
-        }
-    }
-}
+//                // Test Case 2
+//                Console.WriteLine("\nCreating Student 2 (Invalid Age)");
+//                Student student2 = new Student(102, "Modi", 75);
+//                student2.DisplayStudentDetails();
+//            }
+//        }
+//    }
+//}
 
