a7b259a33f95805dd864d3cc9de47a5e99a01e90
akash <akash@DESKTOP-B1PDELG>
2026-05-26 11:15:11 +0530

    Copy Constructor Avoiding Reference Issues

--- OOPS/CopyConstructorEx.cs
+++ OOPS/CopyConstructorEx.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class Student
-    {
-        public int Sid { get; set; }
-        public string Name { get; set; }
-        // Copy constructor
-        public Student(Student other)
-        {
-            Sid = other.Sid;
-            Name = other.Name;
-        }
-        public Student(int sid, string name)
-        {
-            Sid = sid;
-            Name = name;
-        }
-        public void Display()
-        {
-            Console.WriteLine("Student ID: " + Sid);
-            Console.WriteLine("Student Name: " + Name);
-        }
-    }
-    internal class CopyConstructorEx
-    {
-        static void Main(string[] args)
-        {
-            Student student1 = new Student(101, "Alice");
-            Console.WriteLine("Original Student:");
-            student1.Display();
-            // Creating a new student using the copy constructor
-            Student student2 = new Student(student1);
-            Console.WriteLine("\nCopied Student:");
-            student2.Display();
-        }
-    }
-}
+//namespace OOPS
+//{
+//    class Student
+//    {
+//        public int Sid { get; set; }
+//        public string Name { get; set; }
+//        // Copy constructor
+//        public Student(Student other)
+//        {
+//            Sid = other.Sid;
+//            Name = other.Name;
+//        }
+//        public Student(int sid, string name)
+//        {
+//            Sid = sid;
+//            Name = name;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("Student ID: " + Sid);
+//            Console.WriteLine("Student Name: " + Name);
+//        }
+//    }
+//    internal class CopyConstructorEx
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student student1 = new Student(101, "Alice");
+//            Console.WriteLine("Original Student:");
+//            student1.Display();
+//            // Creating a new student using the copy constructor
+//            Student student2 = new Student(student1);
+//            Console.WriteLine("\nCopied Student:");
+//            student2.Display();
+//        }
+//    }
+//}
