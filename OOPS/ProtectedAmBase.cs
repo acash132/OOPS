1e81767e6f4c57036986bee6c0fc437cd67d9196
akash <akash@DESKTOP-B1PDELG>
2026-05-26 10:37:51 +0530

    CopyConstructor Implemented

--- OOPS/ProtectedAmBase.cs
+++ OOPS/ProtectedAmBase.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class Student
-    {
-        protected Student(int Sid)
-        {
-            Console.WriteLine("Protected constructor - Student ID: " + Sid);
-        }
-    }
+//namespace OOPS
+//{
+//    class Student
+//    {
+//        protected Student(int Sid)
+//        {
+//            Console.WriteLine("Protected constructor - Student ID: " + Sid);
+//        }
+//    }
 
-    class CollegeStudent : Student
-    {
-        public CollegeStudent(int Sid) : base(Sid)
-        {
-            Console.WriteLine("CollegeStudent constructor - Student ID: " + Sid);
-        }
-    }
-    internal class ProtectedAmBase
-    {
-        static void Main(string[] args)
-        {
-            CollegeStudent cs = new CollegeStudent(123);
-        }
-    }
-}
+//    class CollegeStudent : Student
+//    {
+//        public CollegeStudent(int Sid) : base(Sid)
+//        {
+//            Console.WriteLine("CollegeStudent constructor - Student ID: " + Sid);
+//        }
+//    }
+//    internal class ProtectedAmBase
+//    {
+//        static void Main(string[] args)
+//        {
+//            CollegeStudent cs = new CollegeStudent(123);
+//        }
+//    }
+//}
