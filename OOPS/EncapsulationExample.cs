8ecda8aa23f1622a93c5f561c40aac9d2170e20c
akash <akash@DESKTOP-B1PDELG>
2026-05-27 10:53:55 +0530

    Encapsulation Account Example checkingPIN

--- OOPS/EncapsulationExample.cs
+++ OOPS/EncapsulationExample.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class Account
-    {
-        private double balance;
-        public void Deposit(double amount)
-        {
-            if (amount > 0)
-            {
-                balance += amount;
-                Console.WriteLine($"Deposited: {amount}, New Balance: {balance}");
-            }
-            else
-            {
-                Console.WriteLine("Deposit amount must be positive.");
-            }
-        }
-        public void Withdraw(double amount)
-        {
-            if (amount > 0 && amount <= balance)
-            {
-                balance -= amount;
-                Console.WriteLine($"Withdrew: {amount}, New Balance: {balance}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid withdrawal amount.");
-            }
-        }
-        public double GetBalance()
-        {
-            return balance;
-        }
-    }
-    internal class EncapsulationExample
-    {
-        static void Main(string[] args)
-        {
-            Account myAccount = new Account();
-            myAccount.Deposit(1000);
-            myAccount.Withdraw(200);
-            Console.WriteLine($"Current Balance: {myAccount.GetBalance()}");
-        }
-    }
-}
+//namespace OOPS
+//{
+//    class Account
+//    {
+//        private double balance;
+//        public void Deposit(double amount)
+//        {
+//            if (amount > 0)
+//            {
+//                balance += amount;
+//                Console.WriteLine($"Deposited: {amount}, New Balance: {balance}");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Deposit amount must be positive.");
+//            }
+//        }
+//        public void Withdraw(double amount)
+//        {
+//            if (amount > 0 && amount <= balance)
+//            {
+//                balance -= amount;
+//                Console.WriteLine($"Withdrew: {amount}, New Balance: {balance}");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid withdrawal amount.");
+//            }
+//        }
+//        public double GetBalance()
+//        {
+//            return balance;
+//        }
+//    }
+//    internal class EncapsulationExample
+//    {
+//        static void Main(string[] args)
+//        {
+//            Account myAccount = new Account();
+//            myAccount.Deposit(1000);
+//            myAccount.Withdraw(200);
+//            Console.WriteLine($"Current Balance: {myAccount.GetBalance()}");
+//        }
+//    }
+//}
