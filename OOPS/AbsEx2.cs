01ae24d626a4ffb7cc1d87f02eac4dac6b4ea449
akash <akash@DESKTOP-B1PDELG>
2026-05-28 11:51:36 +0530

    Abstact principle with Real time Ex

--- OOPS/AbsEx2.cs
+++ OOPS/AbsEx2.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    class myclass
-    {
-        public void display() {
-            Console.WriteLine("Welcome to OOPS")
-                ;
-        }
-        public virtual void show()
-           {
-            Console.WriteLine("Show is calling");
-        }
-    }
-    class dc : myclass
-    {
-        public void message()
-        {
-            Console.WriteLine("Message is Calling");
-        }
-        public override void show()
-        {
-            Console.WriteLine("This is overridden Method");
-        }
-    }
-    internal class AbsEx2
-    {
-        static void main()
-        {
-            myclass obj= new myclass();
-            obj.display();
-            obj.show();
+//namespace OOPS
+//{
+//    class myclass
+//    {
+//        public void display() {
+//            Console.WriteLine("Welcome to OOPS")
+//                ;
+//        }
+//        public virtual void show()
+//           {
+//            Console.WriteLine("Show is calling");
+//        }
+//    }
+//    class dc : myclass
+//    {
+//        public void message()
+//        {
+//            Console.WriteLine("Message is Calling");
+//        }
+//        public override void show()
+//        {
+//            Console.WriteLine("This is overridden Method");
+//        }
+//    }
+//    internal class AbsEx2
+//    {
+//        static void main()
+//        {
+//            myclass obj= new myclass();
+//            obj.display();
+//            obj.show();
 
-            dc objdc= new dc();
-            objdc.message();
-            objdc.show();
-        }
-    }
-}
+//            dc objdc= new dc();
+//            objdc.message();
+//            objdc.show();
+//        }
+//    }
+//}
