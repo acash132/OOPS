74bff6752602163dc492642adfc38f60d8ae943e
akash <akash@DESKTOP-B1PDELG>
2026-05-29 15:12:02 +0530

    Abstract Class with Single inheritance is implemented

---
+++ OOPS/AbstractSingleInheritanceEx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    abstract class Vehicle
+    {
+        protected String VehicleName;
+        public Vehicle(string vname) 
+        {
+            this.VehicleName = vname;
+            Console.WriteLine("Vehicle is called");
+
+        }
+        public void ShowVehicleDetails()
+        {
+            Console.WriteLine($"Vehicle name is {VehicleName}");
+        }
+        public abstract void VehicleType();
+    }
+
+    abstract class VehicleModel : Vehicle
+    {
+        protected String ModelName;
+        public VehicleModel(string Vehiclename, string ModelName) : base(Vehiclename)
+        {
+            this.ModelName = ModelName;
+            Console.WriteLine("VehicleModel is calling");
+
+        }
+        public void DisplayName()
+        {
+            Console.WriteLine($"Model Name is {ModelName}");
+        }
+        public abstract void ModelType();
+    }
+    class VehicleData : VehicleModel
+    {
+        public int VehicleNum;
+        public VehicleData(string Vehiclename, string ModelName,int VehicleNum) : base(Vehiclename, ModelName)
+        {
+            this.VehicleNum = VehicleNum;
+            Console.WriteLine("Vehicle data is called");
+
+        }
+        public override void VehicleType()
+        {
+            Console.WriteLine("Cars");
+        }
+        public override void ModelType()
+        {
+            Console.WriteLine("Electric");
+        }
+    }
+    internal class AbstractSingleInheritanceEx
+    {
+        static void Main()
+        {
+            VehicleData vd = new VehicleData("Tesla", "Avi", 1111);
+            vd.ModelType();
+            vd.VehicleType();
+            vd.DisplayName();
+            vd.ShowVehicleDetails();
+        }
+    }
+}
--- OOPS/Constructor_AbsClass.cs
+++ OOPS/Constructor_AbsClass.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    abstract class Person {
-        protected string name; 
-        private int age;
-        public Person(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-            Console.WriteLine("Person Constructor Called");
-        }
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Name:{name},Age:{age}");
-        }
-        public abstract void Role();
-    }
+//namespace OOPS
+//{
+//    abstract class Person {
+//        protected string name; 
+//        private int age;
+//        public Person(string name, int age)
+//        {
+//            this.name = name;
+//            this.age = age;
+//            Console.WriteLine("Person Constructor Called");
+//        }
+//        public void DisplayInfo()
+//        {
+//            Console.WriteLine($"Name:{name},Age:{age}");
+//        }
+//        public abstract void Role();
+//    }
 
-    class Student:Person 
-    {
-        public int RollNo;
-        public Student(string name, int age, int rollno) : base(name, age)
-        {
-            this.RollNo = rollno;
-            Console.WriteLine("Student constructor called");
+//    class Student:Person 
+//    {
+//        public int RollNo;
+//        public Student(string name, int age, int rollno) : base(name, age)
+//        {
+//            this.RollNo = rollno;
+//            Console.WriteLine("Student constructor called");
 
-        }
+//        }
 
-        public void StudentRoll()
-        {
-            Console.WriteLine("Student Roll no" + RollNo);
-        }
+//        public void StudentRoll()
+//        {
+//            Console.WriteLine("Student Roll no" + RollNo);
+//        }
 
-        public override void Role()
-        {
-            Console.WriteLine("Iam a Student");
-        }
-    }
-    internal class Constructor_AbsClass
-    {
-        static void Main()
-        {
-            Student s = new Student("Akash", 22, 100);
-            s.StudentRoll();
-            s.DisplayInfo();
-            s.Role();
-        }
-    }
-}
+//        public override void Role()
+//        {
+//            Console.WriteLine("Iam a Student");
+//        }
+//    }
+//    internal class Constructor_AbsClass
+//    {
+//        static void Main()
+//        {
+//            Student s = new Student("Akash", 22, 100);
+//            s.StudentRoll();
+//            s.DisplayInfo();
+//            s.Role();
+//        }
+//    }
+//}
