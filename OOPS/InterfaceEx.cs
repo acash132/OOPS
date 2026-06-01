868bad34810a2eabee24434ab8f9ac11e004055b
akash <akash@DESKTOP-B1PDELG>
2026-06-01 12:01:37 +0530

    Interface Default Method

--- OOPS/InterfaceEx.cs
+++ OOPS/InterfaceEx.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace OOPS
-{
-    interface Ishape {
-        void Area();
-        double Perimeter();
-    }
+//namespace OOPS
+//{
+//    interface Ishape {
+//        void Area();
+//        double Perimeter();
+//    }
 
-    public class Circle : Ishape
-    {
-        public double Radius { get; set; }
-        public void Area()
-        {
-            Console.WriteLine("Area of Circle: " + (Math.PI * Radius * Radius));
-        }
-        public double Perimeter()
-        {
-            return 2 * Math.PI * Radius;
-        }
-    }
-    class Rectangle : Ishape
-    {
-        public double Length { get; set; }
-        public double Width { get; set; }
-        public void Area()
-        {
-            Console.WriteLine("Area of Rectangle: " + (Length * Width));
-        }
-        public double Perimeter()
-        {
-            return 2 * (Length + Width);
-        }
-    }
-    internal class InterfaceEx
-    {
-        static void Main()
-        {
-            Circle circle = new Circle { Radius = 5 };
-            circle.Area();
-            Console.WriteLine("Perimeter of Circle: " + circle.Perimeter());
-            Rectangle rectangle = new Rectangle { Length = 4, Width = 6 };
-            rectangle.Area();
-            Console.WriteLine("Perimeter of Rectangle: " + rectangle.Perimeter());
-        }
-    }
-}
+//    public class Circle : Ishape
+//    {
+//        public double Radius { get; set; }
+//        public void Area()
+//        {
+//            Console.WriteLine("Area of Circle: " + (Math.PI * Radius * Radius));
+//        }
+//        public double Perimeter()
+//        {
+//            return 2 * Math.PI * Radius;
+//        }
+//    }
+//    class Rectangle : Ishape
+//    {
+//        public double Length { get; set; }
+//        public double Width { get; set; }
+//        public void Area()
+//        {
+//            Console.WriteLine("Area of Rectangle: " + (Length * Width));
+//        }
+//        public double Perimeter()
+//        {
+//            return 2 * (Length + Width);
+//        }
+//    }
+//    internal class InterfaceEx
+//    {
+//        static void Main()
+//        {
+//            Circle circle = new Circle { Radius = 5 };
+//            circle.Area();
+//            Console.WriteLine("Perimeter of Circle: " + circle.Perimeter());
+//            Rectangle rectangle = new Rectangle { Length = 4, Width = 6 };
+//            rectangle.Area();
+//            Console.WriteLine("Perimeter of Rectangle: " + rectangle.Perimeter());
+//        }
+//    }
+//}
